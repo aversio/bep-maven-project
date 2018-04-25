7c77206007389db21cde921a71f60670be27ec1b
aversio <aversio@protonmail.com>
2018-04-25 11:55:30 +0200

    added fail test

---
+++ test/Testing.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class Testing {
+    @Test
+    public void test_fail() {
+        fail("Let's test the failing unit test");
+    }
+}
