dc3d7c3e2766a605dd72c9d970fcefd926698a12
aversio <aversio@protonmail.com>
2018-05-07 13:21:08 +0200

    removed error and updated sonar in wercker

--- src/main/java/friendspammer/MongoSaver.java
+++ src/main/java/friendspammer/MongoSaver.java
@@ -18,44 +18,39 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class MongoSaver {
-	
-	public static boolean saveEmail(String to, String from, String subject, String text, Boolean html) {
-		String userName = "YOUR NAME";
-		String password = "YOUR PASS";
-		String database = "YOUR DB";
 
-		GEEF ERROR AMK
-		
-		MongoCredential credential = MongoCredential.createCredential(userName, database, password.toCharArray());
-		
-		boolean success = true;
-		
-		try (MongoClient mongoClient = new MongoClient(new ServerAddress("YOUR HOST", 27939), credential, MongoClientOptions.builder().build()) ) {
-			
-			MongoDatabase db = mongoClient.getDatabase( database );
-			
-			MongoCollection<Document> c = db.getCollection("email");
-			
-			Document  doc = new Document ("to", to)
-			        .append("from", from)
-			        .append("subject", subject)
-			        .append("text", text)
-			        .append("asHtml", html);
-			c.insertOne(doc);
-		} catch (MongoException mongoException) {
-			System.out.println("XXXXXXXXXXXXXXXXXX ERROR WHILE SAVING TO MONGO XXXXXXXXXXXXXXXXXXXXXXXXXX");
-			mongoException.printStackTrace();
-			success = false;
-		}
-		
-		return success;
- 		
-	}
-	
-	
-	public static void main(String ...args) throws UnknownHostException {
-		
-		System.out.println("test");
-	}
+    public static boolean saveEmail(String to, String from, String subject, String text, Boolean html) {
+        String userName = "YOUR NAME";
+        String password = "YOUR PASS";
+        String database = "YOUR DB";
+
+        MongoCredential credential = MongoCredential.createCredential(userName, database, password.toCharArray());
+
+        boolean success = true;
+
+        try (MongoClient mongoClient = new MongoClient(new ServerAddress("YOUR HOST", 27939), credential, MongoClientOptions.builder().build())) {
+
+            MongoDatabase db = mongoClient.getDatabase(database);
+
+            MongoCollection<Document> c = db.getCollection("email");
+
+            Document doc = new Document("to", to)
+                    .append("from", from)
+                    .append("subject", subject)
+                    .append("text", text)
+                    .append("asHtml", html);
+            c.insertOne(doc);
+        } catch (MongoException mongoException) {
+            System.out.println("XXXXXXXXXXXXXXXXXX ERROR WHILE SAVING TO MONGO XXXXXXXXXXXXXXXXXXXXXXXXXX");
+            mongoException.printStackTrace();
+            success = false;
+        }
+
+        return success;
+    }
+
+    public static void main(String... args) throws UnknownHostException {
+        System.out.println("test");
+    }
 
 }
